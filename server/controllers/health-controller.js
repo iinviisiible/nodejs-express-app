ecbec7eef51f80cab331a044b76931e6521c0375
myaws1337 <myaws1337@gmail.com>
2020-07-18 10:40:22 -0400

    second change

--- server/controllers/health-controller.js
+++ server/controllers/health-controller.js
@@ -4,4 +4,6 @@ exports.getHealth = (req, res) => {
   res.json({
     status: 'UPPP',
   });
+
+connection.query("SELECT * FROM product WHERE prod = '"+ req.body.prod +"' AND product_name = '"+ req.body.product_name +"'",function(error, results){});
 };
